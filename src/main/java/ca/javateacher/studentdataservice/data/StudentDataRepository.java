063336806c1c1b066129db6c2caa79130bbefaed
Alex Tetervak <tetervak@sheridancollege.ca>
2020-05-18 22:32:38 -0400

    initial commit

---
+++ src/main/java/ca/javateacher/studentdataservice/data/StudentDataRepository.java
@@ -0,0 +1,6 @@
+package ca.javateacher.studentdataservice.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentDataRepository extends JpaRepository<Student, Integer> {
+}
