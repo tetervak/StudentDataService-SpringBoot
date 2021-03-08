0fe7dc4e7d73e41d01d0d5cd37b171cfea400c97
Alex Tetervak <tetervak@sheridancollege.ca>
2021-03-07 22:26:02 -0500

    removed unused imports

--- src/main/java/ca/tetervak/studentdataservice/config/SecurityConfig.java
+++ src/main/java/ca/tetervak/studentdataservice/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package ca.tetervak.studentdataservice.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
